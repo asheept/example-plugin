d79d8d03fbeb8cba09b731916397e09018d24bb9
asheept <asheept@gmail.com>
2022-07-04 15:04:36 +0900

    commit

---
+++ src/main/kotlin/io/github/asheept/KotlinPlugin.kt
@@ -0,0 +1,14 @@
+package io.github.asheept
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KotlinPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+
+        //server.pluginManager.registerEvents(KotlinListener(), this)
+        println("hello")
+        println("hello2")
+        println("hello3")
+    }
+}
